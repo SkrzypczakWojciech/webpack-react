752d9434e0c3651f2b937239622646f0cd6f295c
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-30 17:07:05 +0200

    add title.js

--- components/title.js
+++ components/title.js
@@ -1,3 +1,13 @@
-const Title = props => <h1>{props.title}</h1>
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    return (
+        <div>
+          <h4>Title</h4>
+        </div>
+    );
+  }
+}
 
 export default Title;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/app';
+import Title from './components/title';
 import uuid from 'uuid';
 
+
 ReactDOM.render(
+	<Title />,
     <App />,
     document.getElementById('app')
 );
