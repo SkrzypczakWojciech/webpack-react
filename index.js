71b57714a642f5b37f5180dd650f5335c38e9ab0
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-30 16:19:18 +0200

    init project

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/app';
+import App from './components/title';
+import uuid from 'uuid';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
